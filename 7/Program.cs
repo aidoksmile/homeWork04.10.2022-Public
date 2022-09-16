529b737813690b27577c64722206d3b35068dd9e
Aida <shakharova.aida@yandex.ru>
2022-09-16 18:35:28 +0600

    Написать программу, которая в двумерном массиве заменяет строки на столбцы

---
+++ 7/Program.cs
@@ -0,0 +1,59 @@
+﻿// Написать программу, которая в двумерном массиве заменяет строки на столбцы или сообщить, 
+// что это невозможно (в случае, если матрица не квадратная).
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    Random rand = new Random();
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+            {
+                matr[i,j] = new Random().Next(1,10);
+            }
+    }
+}
+
+void Sort(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = i; j < matr.GetLength(1); j++)
+        {
+            int tmp = matr[i, j];
+            matr[i, j] = matr[j, i];
+            matr[j, i] = tmp;
+        }
+
+    }
+}
+
+Console.WriteLine("Введите размерность массива m*n");
+Console.Write("Введите m: ");
+int m  = int.Parse(Console.ReadLine());;
+Console.Write("Введите n: ");
+int n  = int.Parse(Console.ReadLine());;
+int[,] matrix = new int[m, n];
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+if (matrix.GetLength(0) != matrix.GetLength(1))
+{
+    Console.WriteLine("Матрица не квадратная. Обмен невозможен");
+}
+else
+{
+    Sort(matrix);
+    PrintArray(matrix);
+}
